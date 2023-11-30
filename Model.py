0bf96421500db260453c10f339898dc18bd418b3
Garipyti <clementbarcaroli@hotmail.com>
2023-11-30 11:11:17 +0100

    Premier fichier

--- Model.py
+++ Model.py
@@ -1,45 +1,17 @@
 import pandas as pd
 from abc import ABC, abstractmethod
-pokemon = pd.read_csv("/Users/clementbarcaroli/PycharmProjects/Pokedex/pokemon.csv")
-
-
-#Définition des méthodes de classement des Pokémons
-
-class classement(ABC): #méthode de classement générale qui prend en argument l'attribut de classement
-
-    @abstractmethod
-    def classement(self,attribut):
-        pass
-
-class cd(classement) : #méthode de classement par ordre décroissant
-    def classement(self,attribut):
-        return (pd.DataFrame.sort_values(self,attribut, ascending=False))
-
-class cc(classement) : #méthode de classement par ordre croissant
-    def classement(self,attribut):
-        return (pd.DataFrame.sort_values(self,attribut, ascending=True))
-
-
-#Définition des méthodes de tri des Pokémons
-class filtre(ABC) : #méthode de filtre générale
-
-    @abstractmethod
-    def filtre(self):
-        pass
-
-class filtrec(filtre): ##méthode pour chercher les pokémons avec un attribut = valeur
-    def tri(self, attribut, valeur):
-        return (self[self[attribut] == valeur])
-
-class filtreinf(filtre): #méthode pour chercher les pokémons avec un attribut < valeur
-    def filtre(self,attribut,valeur):
-        return (self[self[attribut] < valeur])
-
-#méthode pour chercher les pokémons avec un attribut > valeur
-class filtresup(filtre):
-    def filtre(self,attribut,valeur):
-        return (self[self[attribut] > valeur])
-
+from tkinter import *
+from tkinter import ttk
+root=Tk()
+cStringVar=StringVar(root,'sensclassement')
+classementC=Radiobutton(root, variable=cStringVar, value="C" )
+classementC.place(x=350,y=100)
+labelclassementC=Label(root,text='croissant')
+labelclassementC.place(x=400,y=100)
+classementD=Radiobutton(root,variable=cStringVar,value="D")
+classementD.place(x=350,y=120)
+labelclassementD=Label(root,text='décroissant')
+labelclassementD.place(x=400,y=120)
 
 
 
--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import pandas as pd
 from abc import ABC, abstractmethod
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from IPython.display import display
 pokemon = pd.read_csv("pokemon.csv") # il y aura peut-être besoin d'enlever les NaN plus tard
 
-##########Model##########
+##########méthodes de tri##########
 class filtre(ABC) : #méthode de filtre générale
 
     @abstractmethod
     def filtre(self):
         pass
 
-class filtrec(filtre): ##méthode pour chercher les pokémons avec un attribut = valeur
-    def tri(self, attribut, valeur):
+class filtrec(filtre): #méthode pour chercher les pokémons avec un attribut = valeur
+    def filtre(self, attribut, valeur):
         return (self[self[attribut] == valeur])
 
 class filtreinf(filtre): #méthode pour chercher les pokémons avec un attribut < valeur
     def filtre(self,attribut,valeur):
-        return (self[self[attribut] < valeur])
+        return self[self[attribut] < valeur]
 
 #méthode pour chercher les pokémons avec un attribut > valeur
 class filtresup(filtre):
@@ -37,17 +39,68 @@ class cc(classement) : #méthode de classement par ordre croissant
     def classement(self,attribut):
         return (pd.DataFrame.sort_values(self,attribut, ascending=True))
 
+
+
+# ex syntaxe : print(filtrec.filtre(pokemon,"Speed",180))
+
 #######Vue########
-class vue(tk.Tk):
-    def __init__(self):
-        tk.Tk.__init__(self)
-        tk.Tk.mainloop(self)
+def affichage():
+##################################################
+    #création fenêtre
+    root=Tk()
+    sdf=pokemon
+    root.geometry('100x100')
+    #root.attributes('-fullscreen',True)
+    root.configure(background='#DB261D')
+
+##################################################
+    #création boutons :
+
+    #Menu déroulant pour choisir critère de classement
+    listeAttribut=list(pokemon.columns)
+    choixClassement=ttk.Combobox(root,values=listeAttribut)
+    def action(event):
+        select = choixClassement.get()
+
+    choixClassement.pack()
+    choixClassement.bind("<<ComboboxSelected>>", action)
+    choixClassement.place(relx=0.1,rely=0.1)
 
 
+    #radio buttons pour choisir si classement croissant ou décroissant
+    cStringVar=StringVar(root,'sensclassement')
+    classementC=Radiobutton(root, variable=cStringVar, value="C" )
+    classementC.place(relx=0.35,rely=0.1)
+    labelclassementC=Label(root,text='croissant')
+    labelclassementC.place(relx=0.4,rely=0.1)
+    classementD=Radiobutton(root,variable=cStringVar,value="D")
+    classementD.place(relx=0.35,rely=0.12)
+    labelclassementD=Label(root,text='décroissant')
+    labelclassementD.place(relx=0.4,rely=0.12)
+
+    #Check button pour choisir le ou les critères de filtre
+    listefiltres=[]
+    def ajoutliste(texte,listefiltres):
+        listefiltres=listefiltres+[str(texte)]
+    def creerCheckbutton(texte,a,b):
+        checkbox=Checkbutton(root, text=str(texte), command=ajoutliste(texte,listefiltres))
+        checkbox.place(relx=a/1000,rely=b/1000)
+
+    for i in range (0,len(listeAttribut)) :
+        texte=str(listeAttribut[i])
+        if i*10 < 500 :
+            a=500
+            b=i*27
+        else :
+            a=600
+            b=i*27 % 500
+        creerCheckbutton(texte,a,b)
 
 
+affichage()
 
 
+mainloop()
 
 
 

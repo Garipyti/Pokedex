3aa71ec04f206fc0ed3dd72c4576303ee8051703
Garipyti <clementbarcaroli@hotmail.com>
2023-12-05 21:44:55 +0100

    Premier fichier

--- main.py
+++ main.py
@@ -51,75 +51,99 @@ def affichage():
     sdf=pokemon
     root.geometry('1000x1000')
     #root.attributes('-fullscreen',True)
-    root.configure(background='#DB261D')
+    #root.configure(background='#5b5a57')
 
 ##################################################
     #création boutons :
-
+    #relwidth=0.4,relheight=0.1
+    frameRadio=Frame(root)
+    frameRadio.place(relx=0,rely=0.1,relwidth=0.4,relheight=0.1)
     #Menu déroulant pour choisir critère de classement
     listeAttribut=list(pokemon.columns)
-    choixClassement=ttk.Combobox(root,values=listeAttribut)
-    def action(event):
+    choixClassement=ttk.Combobox(frameRadio,values=listeAttribut)
+    def action(event): #méthode qui stocke le choix dans le menu déroulant
         select = choixClassement.get()
 
     choixClassement.pack()
     choixClassement.bind("<<ComboboxSelected>>", action)
-    choixClassement.place(relx=0.1,rely=0.1)
-
+    choixClassement.grid(row=1,column=0,sticky=W,columnspan=2)
+    choixClassementLabel=Label(frameRadio,text="Classer selon :")
+    choixClassementLabel.grid(row=0,column=0)
 
     #radio buttons pour choisir si classement croissant ou décroissant
+    labelSensclassement=Label(frameRadio, text="dans l'ordre")
+    labelSensclassement.grid(row=0,column=3,sticky=W,columnspan=2)
     cStringVar=StringVar(root,'sensclassement')
-    classementC=Radiobutton(root, variable=cStringVar, value="C" )
-    classementC.place(relx=0.35,rely=0.1)
-    labelclassementC=Label(root,text='croissant')
-    labelclassementC.place(relx=0.4,rely=0.1)
-    classementD=Radiobutton(root,variable=cStringVar,value="D")
-    classementD.place(relx=0.35,rely=0.12)
-    labelclassementD=Label(root,text='décroissant')
-    labelclassementD.place(relx=0.4,rely=0.12)
+    classementC=Radiobutton(frameRadio, variable=cStringVar, value="C" )
+    classementC.grid(row=1,column=3,sticky=W)
+    labelclassementC=Label(frameRadio,text='croissant')
+    labelclassementC.grid(row=1,column=4,sticky=W)
+    classementD=Radiobutton(frameRadio,variable=cStringVar,value="D")
+    classementD.grid(row=2,column=3,sticky=W)
+    labelclassementD=Label(frameRadio,text='décroissant')
+    labelclassementD.grid(row=2,column=4,sticky=W)
 
     #Check button pour choisir le ou les critères de filtre
     rootHeight = root.winfo_height()
     rootWidth = root.winfo_width()
-    frameCheck = Frame(root,background='grey')
-    frameCheck.place(relx=0.5,rely=0.3,relheight=0.35,relwidth=0.3)
-    frameCheckHeight=frameCheck.winfo_height()
-    frameCheckWidth = frameCheck.winfo_width()
+    frameCheck = Frame(root)
+    frameCheck.place(relx=0.5,rely=0.1,relheight=0.35,relwidth=0.4)
     dicofiltres={}
 
     class checkbutton : #classe de boutons cochables
-        def __init__(self,texte,a,b):
-            self.texte=texte
-            self.a=a
-            self.b=b
+            def __init__(self,texte,a,b,zt,mdRO):
+                self.texte=texte
+                self.a=a
+                self.b=b
+                self.zt=zt
+                self.mdRO=mdRO
+            def creerCheckbutton(self):  # on définit une fonction qui crée et place un bouton cochable et son entrée associée
+                self.cbVar = IntVar()
+                self.ztVar= StringVar()
+                self.mdVar= StringVar()
+                self.dico={self.texte:[self.cbVar,self.ztVar,self.mdVar]} #on crée un item propre à chaque bouton, qui a pour key le nom du filtre et en value une liste avec l'etat et l'attribut de tri
 
-        def creerCheckbutton(self):  # on définit une fonction qui crée et place un bouton cochable et son entrée associée
-            self.cbVar = IntVar()
-            self.ztVar= StringVar()
-            self.dico={self.texte:[self.cbVar,self.ztVar]} #on crée un item propre à chaque bouton, qui a pour key le nom du filtre et en value une liste avec l'etat et l'attribut de tri
-            checkbox = Checkbutton(frameCheck, variable=self.cbVar, text=str(self.texte), offvalue=0, onvalue=1)
-            checkbox.place(relx=self.a, rely=self.b)
-            #def updatedico(self,VarTexte,dicofiltres):
-                #dicofiltres.update(self.dico)
-            #zt=Entry(frameCheck, variable=self.ztVar, command=updatedico(self,self.ztVar,dicofiltres))
-            #zt.place(relx=a+0.1,rely=b)
+                self.checkbox = Checkbutton(frameCheck, variable=self.cbVar, text=str(self.texte), offvalue=0, onvalue=1) #on crée le bouton cochable
+                self.checkbox.place(relx=self.a, rely=self.b)
 
+                def entrymtd(self): #méthode qui met à jour le dico général dès qu'il y a une modif dans la zone de texte
+                    nvlentree=self.zt.get()
+                    self.dico={self.texte:[self.cbVar,nvlentree]}
+                    dicofiltres.update(self.dico)
+                self.zt=Entry(frameCheck, command=entrymtd(self)) #crée la zone de texte associée au bouton
+                self.zt.place(relx=a+0.24,rely=b,relheight=0.08,relwidth=0.1)
 
-
-
+                listeRelOrdre=["<",">","="]
+                self.mdRO = ttk.Combobox(frameCheck, values=listeRelOrdre) #création du menu déroulant des relaitions d'ordre
+                def actionmdRO(event):
+                    mdVar = self.mdRO.get()
+                    self.dico={self.texte:[self.cbVar,self.ztVar,mdVar]}
+                self.mdRO.bind("<<ComboboxSelected>>", actionmdRO)
+                self.mdRO.place(relx=a+0.35,rely=b,relheight=0.07,relwidth=0.08  )
 
     for i in range (0,len(listeAttribut)) : #boucle qui crée autant de boutons cochables qu'il y a de colonnes dans le dataframe
         texte=str(listeAttribut[i])
+        zt=Entry(frameCheck)
+        mdRO=ttk.Combobox(frameCheck)
         if i*0.1 < 1 :
-            a=0.1
+            a=0.05
             b=i*0.1
         else :
-            a=0.6
+            a=0.51
             b=i*0.1 % 1
-        nvbouton=checkbutton(texte,a,b)
+        nvbouton=checkbutton(texte,a,b,zt,mdRO)
         nvbouton.creerCheckbutton()
 
+    #Création du bouton d'affichage du résultat
+    boutonResultat=Label(root,text="Afficher DataFrame")
+    def clique_label():
+        sdf=pokemon
+        print(dicofiltres)
+        for i in dicofiltres:
+            print (i)
 
+    boutonResultat.bind("<Button-1>", clique_label())
+    boutonResultat.place(relx=0.8,rely=0.4)
 affichage()
 
 

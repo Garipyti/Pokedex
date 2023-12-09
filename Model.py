c14a48171a74b67b525c784c2c142ed27a2d6ed8
Garipyti <clementbarcaroli@hotmail.com>
2023-12-09 16:16:34 +0100

    DataFrame affiché

--- Model.py
+++ Model.py
@@ -1,19 +1,119 @@
-import pandas as pd
-from abc import ABC, abstractmethod
-from tkinter import *
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import tkinter as tk
 from tkinter import ttk
-root=Tk()
-cStringVar=StringVar(root,'sensclassement')
-classementC=Radiobutton(root, variable=cStringVar, value="C" )
-classementC.place(x=350,y=100)
-labelclassementC=Label(root,text='croissant')
-labelclassementC.place(x=400,y=100)
-classementD=Radiobutton(root,variable=cStringVar,value="D")
-classementD.place(x=350,y=120)
-labelclassementD=Label(root,text='décroissant')
-labelclassementD.place(x=400,y=120)
+import numpy as np
+import pandas as pd
+
+# Charger les données Pokémon depuis un fichier CSV
+df = pd.read_csv('pokemon.csv', index_col='Name')
+
+# Les attributs que nous voulons inclure dans le graphique radar
+attributs = ['Total', 'HP', 'Attack', 'Defense', 'Speed']
+
+# Normalisation des attributs
+df_normalisé = df.copy()
+for attribut in attributs:
+    df_normalisé[attribut] = df_normalisé[attribut] / df_normalisé[attribut].max()
+
+# Fonction pour créer le graph radar
+# détails à commenter
+def create_radar_plot(pokemons):
+    variables = len(attributs)
+    angles = np.linspace(0, 2 * np.pi, variables, endpoint=False).tolist()
+    angles += angles[:1]
+
+    fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
+
+    for pokemon, couleur in pokemons:
+        valeurs = df_normalisé.loc[pokemon, attributs].tolist()
+        valeurs += valeurs[:1]
+        ax.plot(angles, valeurs, color=couleur, linewidth=1, label=pokemon)
+        ax.fill(angles, valeurs, color=couleur, alpha=0.25)
+
+    ax.set_theta_offset(np.pi / 2)
+    ax.set_theta_direction(-1)
+
+    ax.set_thetagrids(np.degrees(angles[:-1]), attributs)
+
+    for pokemon, angle in zip(ax.get_xticklabels(), angles[:-1]):
+        if angle in (0, np.pi):
+            pokemon.set_horizontalalignment('center')
+        elif 0 < angle < np.pi:
+            pokemon.set_horizontalalignment('left')
+        else:
+            pokemon.set_horizontalalignment('right')
+
+    ax.set_ylim(0, 1)
+    ax.set_rlabel_position(180 / variables)
+
+    plt.legend(loc='upper right')
+    return fig
+
+# Fonction pour afficher le graphique dans Tkinter
+def display_plot_tkinter(fig):
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas_widget = canvas.get_tk_widget()
+    canvas_widget.grid(row=5, column=5, columnspan=3)
+
+# Fonction appelée quan il y le  clic sur le bouton
+def on_button_click():
+    # Récupérer les noms des Pokémon depuis la saisie
+    pokemon1 = entry_pokemon1.get()
+    pokemon2 = entry_pokemon2.get()
+    pokemon3 = entry_pokemon3.get()
+
+    # Liste de Pokémon avec leur couleur associée
+    pokemons = [(pokemon1, '#1aaf6c'), (pokemon2, '#429bf4'), (pokemon3, '#d42cea')]
+
+    # Créer et afficher le graphique radar
+    radar_plot = create_radar_plot(pokemons)
+    display_plot_tkinter(radar_plot)
+
+root = tk.Tk()
+
+# Saisi pour chaque pokémon
+ttk.Label(root, text="Nom Pokémon 1:").grid(row=1, column=0, pady=10)
+entry_pokemon1 = ttk.Entry(root)
+entry_pokemon1.grid(row=1, column=1, pady=10)
+
+ttk.Label(root, text="Nom Pokémon 2:").grid(row=2, column=0, pady=10)
+entry_pokemon2 = ttk.Entry(root)
+entry_pokemon2.grid(row=2, column=1, pady=10)
+
+ttk.Label(root, text="Nom Pokémon 3:").grid(row=3, column=0, pady=10)
+entry_pokemon3 = ttk.Entry(root)
+entry_pokemon3.grid(row=3, column=1, pady=10)
+
+#  bouton pour générer le graphique radar
+button_generate = ttk.Button(root, text="Générer le Graphique Radar", command=on_button_click)
+button_generate.grid(row=4, column=16, pady=10)
+
+root.mainloop()
 
 
+import tkinter as tk
+from tkinter import ttk
+import pandas as pd
+
+# Fonction appelée lorsque le bouton est coché ou décoché
+def on_checkbox_click():
+    value = checkbox_var.get()  # Obtenir la valeur du bouton (1 si coché, 0 sinon)
+    print("La valeur du bouton est :", value)
+
+# Créer une fenêtre principale
+app = tk.Tk()
+app.title("Bouton Cochable")
+
+# Créer une variable Tkinter pour stocker l'état du bouton
+checkbox_var = tk.IntVar()
+
+# Créer un bouton cochable
+checkbox = ttk.Checkbutton(app, text="Cocher pour 1, Décocher pour 0", variable=checkbox_var, command=on_checkbox_click)
+checkbox.grid(column=0, row=0, padx=10, pady=10)
+
+# Lancer la boucle principale
+app.mainloop()
 
 
 
--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from IPython.display import display
 pokemon = pd.read_csv("pokemon.csv") # il y aura peut-être besoin d'enlever les NaN plus tard
 
 ##########méthodes de tri##########
-class filtre(ABC) : #méthode de filtre générale
+class filtre(pd.DataFrame,ABC) : #méthode de filtre générale
 
     @abstractmethod
-    def filtre(self):
+    def filtre(self, attribut, valeur):
         pass
 
 class filtrec(filtre): #méthode pour chercher les pokémons avec un attribut = valeur
@@ -25,7 +25,7 @@ class filtresup(filtre):
     def filtre(self,attribut,valeur):
         return (self[self[attribut] > valeur])
 
-class classement(ABC): #méthode de classement générale qui prend en argument l'attribut de classement
+class classement(pd.DataFrame, ABC): #méthode de classement générale qui prend en argument l'attribut de classement
 
     @abstractmethod
     def classement(self,attribut):
@@ -48,12 +48,70 @@ def affichage():
 ##################################################
     #création fenêtre
     root=Tk()
-    sdf=pokemon
+    root.title("Pokédex")
     root.geometry('1000x1000')
     #root.attributes('-fullscreen',True)
     #root.configure(background='#5b5a57')
+#################################################
+    #création affichage df
+    frameViewer=Frame(root,width=1,height=0.4)
+    frameViewer.place(relx=0,rely=0.6)
+    class DataFrameViewer:
+        instances = []  # Liste pour stocker les instances actives
+        def __init__(self, master, dataframe):
+            self.master = master
 
-##################################################
+            self.dataframe = dataframe
+            self.current_row = 0
+
+            # Création du Treeview pour afficher le DataFrame
+            self.tree = ttk.Treeview(self.master)
+            self.tree["columns"] = list(self.dataframe.columns)
+            self.tree["show"] = "headings"
+
+            for column in self.dataframe.columns:
+                self.tree.heading(column, text=column)
+                self.tree.column(column, width=100)  # Ajustez la largeur des colonnes selon vos besoins
+
+            self.tree.bind("<Up>", self.navigate_up)
+            self.tree.bind("<Down>", self.navigate_down)
+
+            # Remplissage du Treeview avec les données du DataFrame
+            for index, row in self.dataframe.iterrows():
+                self.tree.insert("", "end", values=list(row))
+
+            self.tree.pack(expand=YES, fill=BOTH)
+
+        def navigate_up(self, event):
+            if self.current_row > 0:
+                self.current_row -= 1
+                self.tree.see(self.current_row)
+                self.tree.selection_set(self.current_row)
+                self.tree.focus(self.current_row)
+
+        def navigate_down(self, event):
+            if self.current_row < len(self.dataframe) - 1:
+                self.current_row += 1
+                self.tree.see(self.current_row)
+                self.tree.selection_set(self.current_row)
+                self.tree.focus(self.current_row)
+
+        def update_dataframe(self, new_dataframe):
+            # Met à jour le DataFrame et recharge le Treeview
+            self.dataframe = new_dataframe
+            self.current_row = 0
+
+            for i in self.tree.get_children():
+                self.tree.delete(i)
+
+            for index, row in self.dataframe.iterrows():
+                self.tree.insert("", "end", values=list(row))
+
+        def close(self):
+            # Fermer la fenêtre et retirer l'instance de la liste des instances actives
+            self.master.destroy()
+            DataFrameViewer.instances.remove(self)
+#################################################
     #création boutons :
     #relwidth=0.4,relheight=0.1
     frameRadio=Frame(root)
@@ -91,24 +149,35 @@ def affichage():
     dicofiltres={}
 
     class checkbutton : #classe de boutons cochables
-            def __init__(self,texte,a,b,zt,mdRO):
+            def __init__(self,texte,a,b,zt,mdRO,checkbox):
                 self.texte=texte
                 self.a=a
                 self.b=b
                 self.zt=zt
                 self.mdRO=mdRO
+                self.checkbox=checkbox
             def creerCheckbutton(self):  # on définit une fonction qui crée et place un bouton cochable et son entrée associée
-                self.cbVar = IntVar()
-                self.ztVar= StringVar()
-                self.mdVar= StringVar()
-                self.dico={self.texte:[self.cbVar,self.ztVar,self.mdVar]} #on crée un item propre à chaque bouton, qui a pour key le nom du filtre et en value une liste avec l'etat et l'attribut de tri
+                self.cbVar = BooleanVar(frameCheck)
+                self.ztVar= StringVar(frameCheck,value="")
+                self.mdVar= StringVar(frameCheck,value=">")
+                self.dico={self.texte:[self.cbVar.get(),self.ztVar.get(),self.mdVar.get()]} #on crée un item propre à chaque bouton, qui a pour key le nom du filtre et en value une liste avec l'etat et l'attribut de tri
 
-                self.checkbox = Checkbutton(frameCheck, variable=self.cbVar, text=str(self.texte), offvalue=0, onvalue=1) #on crée le bouton cochable
+                def commandCB():
+                    #nvletat = self.cbVar.get()
+                    print(self.cbVar.get())
+                    self.dico = {self.texte: [self.cbVar.get(), self.ztVar.get(), self.mdVar.get()]}
+                    dicofiltres.update(self.dico)
+                    #print(f"{self.texte} a changé d'état")
+                    #print(self.dico)
+
+                self.checkbox = Checkbutton(frameCheck, variable=self.cbVar, text=str(self.texte), command=commandCB)
+                #self.checkbox = Checkbutton(frameCheck, variable=self.cbVar, text=str(self.texte), offvalue=0, onvalue=1, command=commandCB(self)) #on crée le bouton cochable
                 self.checkbox.place(relx=self.a, rely=self.b)
 
                 def entrymtd(self): #méthode qui met à jour le dico général dès qu'il y a une modif dans la zone de texte
                     nvlentree=self.zt.get()
-                    self.dico={self.texte:[self.cbVar,nvlentree]}
+                    self.dico={self.texte:[self.cbVar.get(),nvlentree,self.mdVar.get()]}
+                    #print(f"{self.texte} a été mis à jour zdt")
                     dicofiltres.update(self.dico)
                 self.zt=Entry(frameCheck, command=entrymtd(self)) #crée la zone de texte associée au bouton
                 self.zt.place(relx=a+0.24,rely=b,relheight=0.08,relwidth=0.1)
@@ -118,32 +187,68 @@ def affichage():
                 def actionmdRO(event):
                     mdVar = self.mdRO.get()
                     self.dico={self.texte:[self.cbVar,self.ztVar,mdVar]}
+                    dicofiltres.update(self.dico)
                 self.mdRO.bind("<<ComboboxSelected>>", actionmdRO)
-                self.mdRO.place(relx=a+0.35,rely=b,relheight=0.07,relwidth=0.08  )
+                self.mdRO.place(relx=a+0.35,rely=b,relheight=0.07,relwidth=0.1)
 
     for i in range (0,len(listeAttribut)) : #boucle qui crée autant de boutons cochables qu'il y a de colonnes dans le dataframe
         texte=str(listeAttribut[i])
         zt=Entry(frameCheck)
         mdRO=ttk.Combobox(frameCheck)
+        checkbox=Checkbutton(frameCheck)
+
         if i*0.1 < 1 :
             a=0.05
             b=i*0.1
         else :
             a=0.51
             b=i*0.1 % 1
-        nvbouton=checkbutton(texte,a,b,zt,mdRO)
+        nvbouton=checkbutton(texte,a,b,zt,mdRO,checkbox)
         nvbouton.creerCheckbutton()
 
     #Création du bouton d'affichage du résultat
-    boutonResultat=Label(root,text="Afficher DataFrame")
-    def clique_label():
-        sdf=pokemon
-        print(dicofiltres)
-        for i in dicofiltres:
-            print (i)
 
-    boutonResultat.bind("<Button-1>", clique_label())
-    boutonResultat.place(relx=0.8,rely=0.4)
+    def clique_bouton():
+        sdf = pokemon.copy()
+        listeobjets = dicofiltres.items()
+        for i in listeobjets:
+            listeauxi = list(i)
+            texte = listeauxi[0]
+            stockinfo = listeauxi[1]
+            #print(str(stockinfo[0]))
+            if stockinfo[0] == True:
+                #print(texte)
+                if stockinfo[2] == "<":
+                    sdf = filtreinf.filtre(sdf, texte, stockinfo[1])
+                    #print("tri inf effectué")
+                    print(sdf)
+                if stockinfo[2] == ">":
+                    print(sdf)
+                    print(texte)
+                    print(stockinfo[1])
+                    sdf = filtresup.filtre(sdf, texte, stockinfo[1])
+                    print(sdf)
+                if stockinfo[2] == "=":
+                    sdf = filtrec.filtre(sdf, texte, stockinfo[1])
+                    print(sdf)
+        CC=str(choixClassement.get())
+        Sens=cStringVar.get()
+        if str(Sens)=="C":
+            sdf=cc.classement(sdf,CC)
+        if str(Sens)=="D":
+            sdf=cd.classement(sdf,CC)
+        #print("It lives !")
+        data_frame_viewer.update_dataframe(sdf)
+    boutonResultat = Button(root, text="Afficher DataFrame", command=clique_bouton)
+    boutonResultat.place(relx=0.8, rely=0.4)
+    data_frame_viewer = DataFrameViewer(frameViewer, pokemon)
+
+
+
+
+#Affichage du DataFrame#
+
+
 affichage()
 
 
@@ -154,6 +259,7 @@ mainloop()
 
 
 
+print(filtrec.filtre(pokemon,"Speed",180))
 
 #def favori(cible):
     #créer sous dataframe
